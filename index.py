65f4594e71c95c7b1fd19697778523b83b2e6285
borko81 <bstoilov81@gmail.com>
2021-03-06 16:26:29 +0200

    Change display in menu settings

--- index.py
+++ index.py
@@ -3,20 +3,40 @@ import fdb
 from base64 import b64encode
 from collections import defaultdict
 
+# ---------Hard code obj id ------------------
+'''
+TODO: add id to url, to generate menu from different id!
+'''
+OBJ_ID = 2
 
+# ----------Flas configuration ---------------
 app = Flask(__name__)
 
+# ---------Database connector ----------------
+con = fdb.connect(dsn=r'C:\Users\Borko\Desktop\noto\touchsale.FDB',
+                  user='SYSDBA', password='masterkey')
 
-con = fdb.connect(dsn=r'path', user='username', password='password')
+# ----------------SQL Selector ---------------------------
+RESTAURANT_NAME = f'''
+    SELECT obj.name_cyr from obj where obj.id = {OBJ_ID}
+'''
 
-SELECT = '''
+SELECT = f'''
 select KITS.NAME_CYR, KINDS.NAME, MENU.CENA, KITS.IMAGE, KITS.INFO
 from KITS
 inner join MENU
 inner join KINDS on KINDS.ID = KITS.KIND_ID on MENU.KIT_ID = KITS.ID
-where MENU.OBJ_ID = 2
+where MENU.OBJ_ID = {OBJ_ID}
 order by 1
 '''
+
+# ----------------Get result from database ----------------
+
+
+def get_obj_name():
+    cur = con.cursor()
+    cur.execute(RESTAURANT_NAME)
+    return cur.fetchone()
 
 
 def get_data_from_database():
@@ -33,15 +53,24 @@ def get_data_from_database():
 
 
 def show_data():
-    result = dict(sorted(get_data_from_database().items()))  
+    result = dict(sorted(get_data_from_database().items()))
     return result
+# ----------------------URL's--------------------------
 
 
-@ app.route('/')
+@app.route('/')
 def index():
-    data = show_data()
+    return "Choice group"
+
+
+@ app.route('/all')
+def all_menu():
+    data = {
+        'recepidata': show_data(),
+        'rest_name': get_obj_name()
+    }
     return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
