17883f26c78bb92573d5cb4814fcdcd32df6c47d
Borko <bstoilov81@gmail.com>
2021-03-13 19:59:33 +0200

    Flexibility in function's

--- index.py
+++ index.py
@@ -31,48 +31,67 @@ def con_to_database():
 # ----------------Get result from database ----------------
 
 
-def get_obj_name():
+def get_cur(query, *args):
+    ''' Return cur with fetch data '''
     with con_to_database() as con:
         cur = con.cursor()
-        cur.execute(queryies['RESTAURANT_NAME'] % (OBJ_ID,))
-        return cur.fetchone()
+        cur.execute(queryies[query] % (*args, ))
+        return cur.fetchall()
+
+
+def get_obj_name(OBJ_ID):
+    ''' 
+        Here return obj name
+        not use any, maybe later!
+    '''
+    return get_cur('RESTAURANT_NAME', OBJ_ID)
 
 
 def get_data_from_database(kinds=None):
-    with con_to_database() as con:
-        cur = con.cursor()
-        if kinds is None:
-            cur.execute(queryies['SELECT'] % (OBJ_ID))
-        else:
-            cur.execute(queryies['SELECT_FROM_KINDS_ID'] % (OBJ_ID, kinds))
-        result = defaultdict(list)
-        for line in cur.fetchall():
-            try:
-                image = b64encode(line[3]).decode("utf-8")
-            except TypeError:
-                image = ''
-            result[line[1]].append([line[0], line[2], image, line[4], line[5]])
-        print(result)
-        return result
+    '''
+        If kinds is none, query not include kinds name, else include.
+    '''
+    result = defaultdict(list)
+
+    if kinds is None:
+        data = get_cur('SELECT', OBJ_ID)
+    else:
+        data = get_cur('SELECT_FROM_KINDS_ID', OBJ_ID, kinds)
+
+    for line in data:
+        try:
+            # If image is none, cathc TypeError and continue with empty image
+            image = b64encode(line[3]).decode("utf-8")
+        except TypeError:
+            image = ''
+        result[line[1]].append([line[0], line[2], image, line[4], line[5]])
+    return result
 
 
 def get_kinds_from_database():
-    with con_to_database() as con:
-        cur = con.cursor()
-        cur.execute(queryies['TAKE_KINDS'] % (OBJ_ID))
-        result = {}
-        for line in cur.fetchall():
-            try:
-                image = b64encode(line[2]).decode("utf-8")
-            except TypeError:
-                image = ''
-            result[line[1]] = [line[0], image, line[3]]
-        return dict(sorted(result.items()))
+    ''' 
+        Return kinds
+    '''
+    result = {}
+
+    data = get_cur('TAKE_KINDS', OBJ_ID)
+    for line in data:
+        try:
+            image = b64encode(line[2]).decode("utf-8")
+        except TypeError:
+            image = ''
+        result[line[1]] = [line[0], image, line[3]]
+    return dict(sorted(result.items()))
 
 
 def show_data(kinds=None):
+    '''
+        Use to return sorted alpha (name kinds), recepi
+        Duck typing
+    '''
     result = dict(sorted(get_data_from_database(kinds=kinds).items()))
     return result
+
 # ----------------------URL's--------------------------
 
 
@@ -86,19 +105,19 @@ def index():
 def load_kinds(id):
     data = {
         'recepidata': show_data(kinds=id),
-        'rest_name': get_obj_name()
+        'rest_name': get_obj_name(OBJ_ID)
     }
     return render_template('all_menu.html', data=data)
 
 
-@ app.route('/all', methods=['GET'])
+@app.route('/all', methods=['GET'])
 def all_menu():
     data = {
         'recepidata': show_data(),
-        'rest_name': get_obj_name()
+        'rest_name': get_obj_name(OBJ_ID)
     }
     return render_template('all_menu.html', data=data)
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=False, threaded=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True)
